cc1b694972e4726377ce81c31b424d5556daa766
s1awek <elvizky@gmail.com>
2016-10-21 20:58:12 +0200

    First working version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function () {
     function doIt(searchTerm) {
-        var urlBase = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=20&prop=pageimages|extracts&pilimit=max&pithumbsize=300&exintro&explaintext&exsentences=2&exlimit=max&origin=*&gsrsearch=' +
+        var urlBase = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=20&prop=pageimages|extracts&pilimit=max&pithumbsize=300&exintro&explaintext&exchars=200&exlimit=max&origin=*&gsrsearch=' +
             searchTerm;
         $.ajax({
             url: urlBase,
@@ -31,33 +31,40 @@ $(function () {
                 var counter = 0;
                 for (key in data['query']['pages']) {
 
-                    if (data['query']['pages'][key]['pageimage'] === undefined) {
+                    if (data['query']['pages'][key]['pageimage'] === undefined) { //skip positions without image
                         continue;
                     } else {
-
+                        document.getElementsByClassName('error')[0].innerHTML = '';
                         var extractTxt = document.createTextNode(data['query']['pages'][key]['extract']);
                         var titleTxt = document.createTextNode(data['query']['pages'][key]['title']);
+                        var titleTxtBig = document.createTextNode(data['query']['pages'][key]['title']);
                         var imgSrcTxt = data['query']['pages'][key]['thumbnail']['source'];
                         var emptyTxtNode = document.createTextNode('');
+                        var artLinkId = data['query']['pages'][key]['pageid'];
                         addNode('li', emptyTxtNode, 'articles-list', 'list', 0, 0);
                         addNode('div', emptyTxtNode, 'list', 'cont', counter, 0);
                         addNode('div', emptyTxtNode, 'cont', 'img-cont', counter, 0);
                         addNode('img', emptyTxtNode, 'img-cont', 'thumb', counter, 1);
                         addNode('div', emptyTxtNode, 'cont', 'cont-ext', counter, 0);
-                        $('.thumb_' + counter).attr("src", imgSrcTxt).addClass('thumb'); //adds src attributte to image and classes .thumb_(inx) and .thumb
+                        addNode('div', emptyTxtNode, 'cont', 'left-cover', counter, 0);
+                        addNode('div', emptyTxtNode, 'cont', 'right-cover', counter, 0);
+                        addNode('h1', titleTxtBig, 'cont', 'cover-title', counter, 2);
+                        $('.thumb_' + counter).attr('src', imgSrcTxt).addClass('thumb'); //adds src attributte to image and classes .thumb_(inx) and .thumb
                         $('.cont').addClass('clearfix');
-                        addNode('h3', titleTxt, 'cont-ext', 'title', counter, 3);
+                        addNode('a', emptyTxtNode, 'cont-ext', 'link', counter, 1);
+                        $('.link_' + counter).attr('href', 'https://en.wikipedia.org/?curid=' + artLinkId).addClass('title-link').attr('target', '_blank');
+                        addNode('h3', titleTxt, 'title-link', 'title', counter, 2);
                         addNode('span', extractTxt, 'cont-ext', 'extract', counter, 0);
                         counter = counter + 1;
+                        console.log(Object.keys(data['query']['pages']).length);
                     }
                 }
             } else {
-                //TODO add nice error handler class
-                document.getElementsByClassName('articles-list')[0].innerHTML = 'error';
+                                document.getElementsByClassName('error')[0].innerHTML = 'There is no articles to show. Please try again.';
             }
 
         }
-
+        //TODO add error handling class and HTML markup
         function errHandler(err) {
             console.log('error: ', err.responseText);
             document.getElementsByTagName('body')[0].innerHTML = err.responseText;
